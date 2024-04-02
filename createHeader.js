d44e3e74bd31e579482db654f35104761f6cb7ca
Bibo00 <bibinileonardo2@gmail.com>
2024-04-02 12:45:33 +0200

    new header, created using javascript

---
+++ createHeader.js
@@ -0,0 +1,59 @@
+// Creazione degli elementi del menu
+const nav = document.createElement('nav');
+const navbar = document.createElement('div');
+navbar.classList.add('navbar');
+const container = document.createElement('div');
+container.classList.add('container', 'nav-container');
+const checkbox = document.createElement('input');
+checkbox.classList.add('checkbox');
+checkbox.setAttribute('type', 'checkbox');
+checkbox.setAttribute('id', 'checkbox');
+const hamburgerLines = document.createElement('div');
+hamburgerLines.classList.add('hamburger-lines');
+for (let index = 1; index < 4; index++) {
+  const line = document.createElement('span');
+  line.classList.add('line', 'line' + index);
+  hamburgerLines.appendChild(line);
+}
+const logo = document.createElement('div');
+logo.classList.add('logo');
+const h1 = document.createElement('h1');
+h1.textContent = 'Attilio';
+const menuItems = document.createElement('div');
+menuItems.classList.add('menu-items');
+const numMenuItems = 4;
+for (let index = 0; index < numMenuItems; index++) {
+  const link = document.createElement('li');
+  const anchor = document.createElement('a');
+  switch(index){
+    case 0:
+      anchor.setAttribute('href', 'index.html');
+      anchor.textContent = 'Home';
+      break;
+    case 1:
+      anchor.setAttribute('href', 'menu.html');
+      anchor.textContent = 'Risorante';
+      break;
+    case 2:
+      anchor.setAttribute('href', '#');
+      anchor.textContent = 'Recensioni';
+      break;
+    case 3:
+      anchor.setAttribute('href', '#');
+      anchor.textContent = 'Chi siamo?';
+      break;
+  }
+  link.appendChild(anchor);
+  menuItems.appendChild(link);
+}
+logo.appendChild(h1);
+container.appendChild(checkbox);
+container.appendChild(hamburgerLines);
+container.appendChild(logo);
+container.appendChild(menuItems);
+navbar.appendChild(container);
+nav.appendChild(navbar);
+
+const firstChild = document.body.firstChild;
+document.body.insertBefore(nav, firstChild);
+
--- header.js
+++ header.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var menuIcon = document.getElementById('menu-icon');
-    var nav = document.querySelector('.nav');
+    var menuIcon = document.getElementById('checkbox');
+    var container = document.querySelector('.container-tot');
 
     menuIcon.addEventListener('click', function() {
-        if (nav.classList.contains('side-menu')) {
-            nav.classList.remove('side-menu');
-            nav.classList.add('hide');
-            
-        } else {
-            nav.classList.remove('hide');
-            nav.classList.add('side-menu');
-            menuIcon.classList.add('hide');
-        }
+            if (container.classList.contains('hide')) {
+                container.classList.remove('hide');
+            } else {
+                container.classList.add('hide');
+            }
     });
 
-    document.addEventListener('click', function(event) {
+   /* document.addEventListener('click', function(event) {
         var isMenuIconClicked = menuIcon.contains(event.target);
         if (!isMenuIconClicked) {
             menuIcon.classList.remove('hide');
             nav.classList.add('hide');
         }
-    });
+    });*/
 });
